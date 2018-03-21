00e651ec9ddc0cb6be92d868d651e1da89a37721
arjunshah1993 <arjun.shah1993.2312@gmail.com>
2018-03-20 17:55:02 -0700

    added factorial while

--- factwhile.py
+++ factwhile.py
@@ -1,10 +1,11 @@
 n = 6
 
-temp1 = 1
+def factwhile(n):
+	temp1 = 1	
+	while n != 1 and n != 0:
+		temp2 = n * (n-1)
+		temp1 = temp1 * temp2
+		n -= 2
+	return(print(temp1)
 
-while n != 1 and n != 0:
-	temp2 = n * (n-1)
-	temp1 = temp1 * temp2
-	n -= 2
-
-print(temp1)
+factwhile(n)
