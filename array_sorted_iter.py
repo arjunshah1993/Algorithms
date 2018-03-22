0193dbed0fcebf54ccef5443541a1e9beca617a1
arjunshah1993 <arjun.shah1993.2312@gmail.com>
2018-03-21 22:46:49 -0700

    corrected corner cases

--- array_sorted_iter.py
+++ array_sorted_iter.py
@@ -1,13 +1,14 @@
-import numpy as np
-arr = np.array([1,1,2,2,3,3])
+arr = [1, 2, 3, 4]
 
 
 def arr_sorted(array):
-    for i in range(0, len(array)-1):
-        if array[i] > array[i+1]:
-            return "Not sorted"
-        else:
-            return "Sorted"
+    flag = False
+    for i in range(0, len(array) - 1):
+        if array[i] > array[i + 1]:
+            flag = True
+    if flag:
+        return "Not sorted"
+    return "Sorted"
 
 
 print(arr_sorted(arr))
