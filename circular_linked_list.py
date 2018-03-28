105edfe80e09f0892bb8b02df68365affa3f92a1
arjunshah1993 <arjun.shah1993.2312@gmail.com>
2018-03-28 12:27:45 -0700

    corrected for corner cases

--- circular_linked_list.py
+++ circular_linked_list.py
@@ -11,26 +11,28 @@ def create_circular_linked_list():
     node2 = Node(2)
     node3 = Node(3)
     node4 = Node(4)
+    node5 = Node(5)
     head.next = node2
     node2.next = node3
     node3.next = node4
-    # node4.next = head
+    node4.next = node5
+    node5.next = node2
     return head
 
 
 def check_loop(head):
-    temp = head
+    slow = head.next
+    fast = slow.next.next
     while head:
-        if head.next:
-            head = head.next
-            if temp == head:
-                print("Loop exists")
-                break
-            else:
-                continue
-        else:
+        if slow == fast:
+            print("Loop exists")
+            break
+        elif slow is None or fast is None:
             print("Loop does not exist")
             break
+        else:
+            slow = slow.next
+            fast = fast.next.next
 
 
 head = create_circular_linked_list()
